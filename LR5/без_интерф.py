230a2d28f69338077a448355b2ce222b1ed4edae
iamgirya <84345515+iamgirya@users.noreply.github.com>
2023-03-17 13:13:44 +0300

    Переход на стейт машину

--- LR5/без_интерф.py
+++ LR5/без_интерф.py
@@ -1,11 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum 
+
+def intervalsToTimeArray(arr):
+    newArr = np.zeros(len(arr))
+    for i in range(len(arr)):
+        for j in range(i+1):
+            newArr[i]+=arr[j]
+    return newArr
+
+States = Enum('States', 'waiting setup execute repair')
 
 # Количество деталей для обработки
 n = 500
 
 # Интервалы между заданиями, распределенные экспоненциально
 intervals = np.random.exponential(scale=1.0, size=n)
+intervals = intervalsToTimeArray(intervals)
 
 # Время наладки станка, равномерно распределенное на интервале 0.2-0.5 часа
 setup_times = np.random.uniform(low=0.2, high=0.5, size=n)
@@ -14,60 +25,68 @@ setup_times = np.random.uniform(low=0.2, high=0.5, size=n)
 task_times = np.random.normal(loc=0.5, scale=0.1, size=n)
 
 # Интервалы между поломками, нормально распределенные с мат. ожиданием 20 часов и среднеквадратичным отклонением 2 часа
-break_intervals = np.random.normal(loc=20, scale=2, size=n)
+break_intervals = np.random.normal(loc=20, scale=2, size=n*2)
+break_intervals = intervalsToTimeArray(break_intervals)
 
 # Время устранения поломки, равномерно распределенное на интервале от 0.1 до 0.5 часа
-repair_times = np.random.uniform(low=0.1, high=0.5, size=n)
+repair_times = np.random.uniform(low=0.1, high=0.5, size=n*2)
 
-# Массивы для хранения времени начала и окончания каждого задания и поломки
-start_times = np.zeros(n)
-end_times = np.zeros(n)
-break_end_times = np.zeros(n)
+# переменные для вывода статистики
+time_of_non_work = 0
+intervals_between_start_task = np.zeros(n)
+execute_time = np.zeros(n)
+start_task_time = np.zeros(n)
 
+# основные рабочие переменные
 current_time = 0
-total_time = 0
-completion_times = []
+state = States.waiting
+task_number = 0
+break_number = 0
+while True:
+    if state == States.waiting:
+        if (task_number == len(intervals)):
+            # выполнение завершено
+            break
+        # если нужно ждать - ждём
+        if (current_time < intervals[task_number]):
+            time_of_non_work += intervals[task_number] - current_time
+            current_time =  intervals[task_number]
+        
+        start_task_time[task_number] = current_time
+        intervals_between_start_task[task_number] = current_time - intervals_between_start_task[task_number-1]
 
-for i in range(n):
-    # Рассчитываем время начала задания, учитывая наладку станка и время ожидания, если задание уже началось, но станок еще занят
-    start_times[i] = current_time + setup_times[i] + max(0, end_times[i-1] - current_time) if i > 0 else current_time + setup_times[i]
-    # Рассчитываем время окончания задания
-    end_times[i] = start_times[i] + task_times[i]
-    # Рассчитываем время следующего задания
-    next_task_time = start_times[i] + intervals[i]
-    # Проверяем, произошла ли поломка перед выполнением задания
-    if next_task_time < break_end_times[i-1]:
-    # Если поломка произошла, то перемещаем текущее задание в начало очереди
-        start_times = np.insert(start_times, i, start_times[i])
-        end_times = np.insert(end_times, i, end_times[i])
-        intervals = np.insert(intervals, i, intervals[i])
-        task_times = np.insert(task_times, i, task_times[i])
-        break_end_times = np.insert(break_end_times, i, break_end_times[i-1] + np.random.uniform(0.1, 0.5))
-        # Задание перемещено в начало, удаляем его из текущей позиции
-        start_times = np.delete(start_times, i+1)
-        end_times = np.delete(end_times, i+1)
-        intervals = np.delete(intervals, i+1)
-        task_times = np.delete(task_times, i+1)
-        break_end_times = np.delete(break_end_times, i+1)
-        # Для следующей итерации i смещается на 1 назад, чтобы повторно не проверять перемещенное задание
-        i -= 1
-    else:
-        # Если поломки нет, то настраиваем станок и выполняем задание
-        setup_time = np.random.uniform(0.2, 0.5)
-        task_time = np.random.normal(0.5, 0.1)
-        # Добавляем время наладки и выполнения задания к времени следующего задания
-        next_task_time += setup_times[i] + task_times[i]
-        # Добавляем время выполнения задания к общему времени работы станка
-        total_time += setup_times[i] + task_times[i]
-        # Записываем время окончания выполнения задания
-        completion_times.append(end_times[i])
-        break_end_times = np.insert(break_end_times, i, break_end_times[i-1] + np.random.uniform(0.1, 0.5))
+        state = States.setup
+    elif state == States.setup:
+        # если станок сломается на этапе подготовки
+        if (current_time + setup_times[task_number] > break_intervals[break_number]):
+            current_time = break_intervals[break_number]
+            state = States.repair
+        else:
+            current_time += setup_times[task_number]
+            state = States.execute
+    elif state == States.execute:
+        # если станок сломается на этапе выполнения
+        if (current_time + task_times[task_number] > break_intervals[break_number]):
+            current_time = break_intervals[break_number]
+            state = States.repair
+        else:
+            current_time += task_times[task_number]
 
-print(f"Общее время работы станка: {total_time:.2f} ч.")
-print(f"Процент времени простоя станка: {(1 - total_time/500)*100:.2f}%")
-print(f"Среднее время между заданиями: {np.mean(intervals):.2f} ч.")
-print(f"Среднее время выполнения задания: {np.mean(task_times):.2f} ч.")
-print(f"Максимальное время ожидания задания: {(max(np.array(completion_times) - np.array(start_times))):.2f} ч.")
+            execute_time[task_number] = current_time - start_task_time[task_number]
+
+            task_number+=1
+            state = States.waiting
+    elif state == States.repair:
+        time_of_non_work += repair_times[break_number]
+        current_time += repair_times[break_number]
+        break_number+=1
+        state = States.setup
+
+print(f"Общее время работы станка: {current_time:.2f} ч.")
+print(f"Времени простоя станка: {time_of_non_work:.2f} ч.")
+print(f"Среднее время между заданиями: {np.mean(intervals_between_start_task[1:]):.2f} ч.")
+print(f"Среднее время выполнения задания: {np.mean(execute_time):.2f} ч.")
+print(f"Максимальное между заданиями: {(max(intervals_between_start_task[1:])):.2f} ч.")
 
 fig, ax = plt.subplots(figsize=(10, 5))
 ax.plot(range(n), intervals, label='Время между заданиями')
